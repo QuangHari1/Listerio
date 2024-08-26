493d974d3632666be3781120341166bc3e648eb4
QuangHari <167546247+QuangHari1@users.noreply.github.com>
2024-08-26 18:10:11 +0700

    Initial commit v2

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/Home/index.vue';
 import Author from '../views/Author/index.vue';
 import Mainpage from '../views/Mainpage/index.vue';
@@ -226,7 +226,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
